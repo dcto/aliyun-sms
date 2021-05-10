2d303b27c90ecfed763cf7254a52dc2026165a86
陶之11 <sdoz@live.com>
2021-05-10 16:41:15 +0800

    push the first version

--- src/Sms.php
+++ src/Sms.php
@@ -11,8 +11,9 @@ class Sms {
     protected $config = array(
         'accessKeyId' => null,
         'accessSecret'=> null,
-        'host'        => 'dysmsapi.ap-southeast-1.aliyuncs.com',
-        'action'      => 'SendMessageToGlobe',
+        'action'      => null,
+        'host'        => 'dysmsapi.cn-hangzhou.aliyuncs.com',
+        'from'        => '短信签名',//送方标识。发往中国传入签名，请在控制台申请短信签名；发往非中国地区传入senderId。
         'regionId'    => 'cn-hangzhou',
         'version'     => '2018-05-01',
     );
@@ -28,15 +29,6 @@ class Sms {
         $this->config = array_merge($this->config, $config);
     }
 
-
-    /**
-     * setting host
-     */
-    public function host($host)
-    {
-        return $this->config('host', $host);
-    }
-
     /**
      * config setting
      */
@@ -44,12 +36,31 @@ class Sms {
     {
         if(is_array($key)){
             $this->config = array_merge($this->config, $key);
+            return $this;
         }else if($value){
             $this->config[$key] = $value;
+            return $this;
+        }else if($key){
+            return isset($this->config[$key]) ? $this->config[$key] : null;
         }else{
             return $this->config;
         }
+        return $this;
+    }
 
+    /**
+     * set parameters 
+     * @return $this
+     */
+    public function query($key = null, $value = null)
+    {
+        if(is_null($key) && is_null($value)) return $this->query;
+        if(is_null($value)) return isset($this->query[$key]) ? $this->query[$key] : null;
+        if(is_array($key)){
+            $this->query = array_merge_recursive($this->query, $key);
+        }else if(!is_null($value)){
+            $this->query[$key] = $value;
+        }
         return $this;
     }
 
@@ -64,82 +75,100 @@ class Sms {
     /**
      * template param alias name
      */
-    public function with($key, $value = null)
+    public function with($key = null, $value = null)
     {
-       return $this->query($key, $value);
+        return $this->query($key, $value);
     }
 
-    /**
-     * set parameters 
-     */
-    public function query($key, $value = null)
-    {
-        if(is_array($key)){
-            $this->query = array_merge($this->config, $key);
-        }else if($value){
-            $this->query[$key] = $value;
-        }else{
-            return $this->query;
-        }
-        return $this;
-    }
 
     /**
      * 
      * @param mixed $action 
      * @return array|$this 
      */
-    public function action($action)
+    public function action($action = 'SendMessageToGlobe')
     {
         return $this->config('action', $action);
     }
 
     /**
-     * From number
-     * @param mixed $number 
+     * From signature
+     * @param mixed $signature 
      * @return array|$this 
      */
-    public function from($number)
+    public function from($signature)
     {
-        return $this->with('From', $number);
+        return $this->query('From', $signature);
     }
 
     /**
      * set phone number
+     * @return $this
      */
     public function phone($number)
     {
-        $this->with('To', $number);
+        $this->query('To', $number);
         return $this;
     }
 
     /**
      * set template code
+     * @return $this
      */
-    public function batch($template, $parameters = null)
+    public function batch($template, array $parameters = array())
     {
-        $this->config('TemplateCode', $template);
-        $parameters && $this->config('TemplateParam', $parameters);
+        $this->template($parameters, $template);
         return $this;
     }
 
+    /**
+     * send with message content
+     * @param mixed $content 
+     * @return $this 
+     */
     public function message($content)
     {
-        return $this->with('Message', $content);
+        $this->action('SendMessageToGlobe');
+        return $this->query('Message', $content);
+    }
+
+    /**
+     * send with template content
+     * @param mixed $template 
+     * @param array $parameters 
+     * @return mixed 
+     */
+    public function template(array $TemplateParam = array(), $TemplateCode = false)
+    {
+        $this->action('SendMessageWithTemplate');
+        return $this->query('TemplateCode', $TemplateCode ?: $this->config('defaultTemplate') )
+        ->query('TemplateParam', json_encode($TemplateParam));
+        
     }
 
     /**
      * send sms
-     * @return bool
+     * @return \AlibabaCloud\Client\Result\Result
      */
-    public function send($phone = null, $message)
+    public function send($phone = null, $content = null)
     {
-        return $this->with('To', $phone)->with('Message', $message)->client()->request();
+        $phone && $this->phone($phone);
+        if($content){
+            is_array($content) ? $this->template($content, $this->config('deafultTemplate')) : $this->message($content);
+        }
+        if($this->query('TemplateCode')){
+            $this->from($this->config('from'));
+            $this->action('SendMessageWithTemplate');
+        }else{
+            $this->query('senderId');
+            $this->action('SendMessageToGlobe');
+        }
+        return $this->client()->request();
     }
 
     /**
-     * 
-     * @return void 
+     * send rpc alibaba clound message
+     * @return \AlibabaCloud\Client\Request\RpcRequest 
      */
     public function client()
     {
@@ -147,8 +176,8 @@ class Sms {
         // Usage：https://github.com/aliyun/openapi-sdk-php-client/blob/master/README-CN.md
         AlibabaCloud::accessKeyClient($this->config('accessKeyId'), $this->config('accessSecret'))
                         ->regionId($this->config('regionId')) 
-                        ->asGlobalClient();
-        return AlibabaCloud::rpcRequest()->product('Dysmsapi')
+                        ->asDefaultClient();
+        return AlibabaCloud::rpc()->product('Dysmsapi')
                             ->host($this->config('host'))
                             ->version($this->config('version'))
                             ->action($this->config('action'))
